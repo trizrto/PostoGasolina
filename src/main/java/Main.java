802ce90233a190dfc3e56b092272bb660a8f2976
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-02 12:25:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     while (numero != ){
+       if(numero !={
+       } System.out.println("digite um numero existente.")
+         
+    System.out.println("Digite 1 para alcool, 2 para gasolina, 3 para diesel e 4 para finalizar o atendimento");
+    String numero = sc.next();
+    if (numero.equals("1")){
+      System.out.println("alcool");
+    }
+    if (numero.equals("2")){
+      System.out.println("gasolina");
+    }
+    if (numero.equals("3")){
+      System.out.println("diesel");
+    }
+    if (numero.equals("4")){
+      System.out.println("fim");
+      
+    }
+    
+     }
+
+
+
+
+
+    sc.close();
+  }
+
+}
